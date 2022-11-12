e6526826cd89f55743e3da1287cb7b9449828c57
Emanuel Allaria <allariaemanuel@gmail.com>
2022-11-12 18:13:57 -0300

    1.2

--- js/comprar_bingo.js
+++ js/comprar_bingo.js
@@ -1,21 +1,21 @@
-let comprarBingo = document.querySelector("#comprar");
+// let comprarBingo = document.querySelector("#comprar");
+
+// // comprarBingo.addEventListener("click", function() {
+// //     alert("hola");
+// // })
+
 
 // comprarBingo.addEventListener("click", function() {
-//     alert("hola");
+//     let nameComprador = document.querySelector(".container_comprar_bingo form #name").value
+//     let dniComprador = document.querySelector("#dni").value
+//     let direccionComprador = document.querySelector("#direccion").value
+//     let localidadComprador = document.querySelector("#localidad").value
+//     let provinciaComprador = document.querySelector("#provincia").value
+//     let telefonoComprador = document.querySelector("#telefono").value
+//     let emailComprador = document.querySelector("#email").value
+//     let nosEncontroComprador = document.querySelector("#c_nos_encontro").value
+
+//     let urlWsp = "https://api.whatsapp.com/send?phone=5493425245550&text=Mensaje de Comprador del Bingo: %0A%0ANombre: %0A" + nameComprador + "%0A%0AD.N.I: %0A" + dniComprador + "%0A%0ADirección: %0A" + direccionComprador + "%0A%0ALocalidad: %0A" + localidadComprador + "%0A%0AProvincia: %0A" + provinciaComprador + "%0A%0ATelefono: %0A" + telefonoComprador + "%0A%0AEmail: %0A" + emailComprador + "%0A%0ANos Encontró por: %0A" + nosEncontroComprador
+
+//     window.open(urlWsp)
 // })
-
-
-comprarBingo.addEventListener("click", function() {
-    let nameComprador = document.querySelector(".container_comprar_bingo form #name").value
-    let dniComprador = document.querySelector("#dni").value
-    let direccionComprador = document.querySelector("#direccion").value
-    let localidadComprador = document.querySelector("#localidad").value
-    let provinciaComprador = document.querySelector("#provincia").value
-    let telefonoComprador = document.querySelector("#telefono").value
-    let emailComprador = document.querySelector("#email").value
-    let nosEncontroComprador = document.querySelector("#c_nos_encontro").value
-
-    let urlWsp = "https://api.whatsapp.com/send?phone=5493425245550&text=Mensaje de Comprador del Bingo: %0A%0ANombre: %0A" + nameComprador + "%0A%0AD.N.I: %0A" + dniComprador + "%0A%0ADirección: %0A" + direccionComprador + "%0A%0ALocalidad: %0A" + localidadComprador + "%0A%0AProvincia: %0A" + provinciaComprador + "%0A%0ATelefono: %0A" + telefonoComprador + "%0A%0AEmail: %0A" + emailComprador + "%0A%0ANos Encontró por: %0A" + nosEncontroComprador
-
-    window.open(urlWsp)
-})
